253cdf8b4bab98973c3dae9a58eb1139f12d73bc
Haci11 <46707887+Haci11@users.noreply.github.com>
2023-08-17 23:05:14 +0200

    Added stlying on user profile

--- src/components/User/User.tsx
+++ src/components/User/User.tsx
@@ -1,21 +1,27 @@
 import React from "react";
+import "./User.css";
 
 const User = ({ playerData }: any) => {
   return (
     <section>
       <ul>
         {playerData && (
-          <div>
-            <h2>Player Information</h2>
-            <img
-              src={`http://ddragon.leagueoflegends.com/cdn/13.13.1/img/profileicon/${playerData.profileIconId}.png`}
-              alt="profile"
-              width={100}
-              height={100}
-            />
-            <p>Name: {playerData.name}</p>
-            <p>Level: {playerData.summonerLevel}</p>
-            {/* Display other player information as needed */}
+          <div className="user--main">
+            <div className="user--profile">
+              <div className="user--level"> {playerData.summonerLevel}</div>
+              <div className="user--img">
+                <img
+                  src={`http://ddragon.leagueoflegends.com/cdn/13.13.1/img/profileicon/${playerData.profileIconId}.png`}
+                  alt="profile"
+                  width={100}
+                  height={100}
+                />
+              </div>
+            </div>
+            <div className="user--info">
+              <p>Name: {playerData.name}</p>
+              {/* Display other player information as needed */}
+            </div>
           </div>
         )}
       </ul>
