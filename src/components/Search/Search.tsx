c38ad8dc59d6876366c5d95eae483b0cc1b9c2a1
Haci11 <46707887+Haci11@users.noreply.github.com>
2023-08-18 19:36:34 +0200

    Added styling for region button

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -16,30 +16,32 @@ const Search = ({ onSubmit }: any) => {
   return (
     <section>
       <form onSubmit={handleSubmit} className="search">
-        <select value={region} onChange={(e) => setRegion(e.target.value)}>
-          <option value="br1">BR1</option>
-          <option value="eun1">EUN1</option>
-          <option value="euw1">EUW1</option>
-          <option value="jp1">JP1</option>
-          <option value="kr">KR</option>
-          <option value="la1">LA1</option>
-          <option value="la2">LA2</option>
-          <option value="na1">NA1</option>
-          <option value="oc1">OC1</option>
-          <option value="tr1">TR1</option>
-          <option value="ru">RU</option>
-          <option value="ph2">PH2</option>
-          <option value="sg2">SG2</option>
-          <option value="th2">TH2</option>
-          <option value="tw2">TW2</option>
-          <option value="vn2">VN2</option>
-        </select>
-        <input
-          type="text"
-          value={summonerName}
-          onChange={(e) => setSummonerName(e.target.value)}
-          placeholder="Enter summoner name"
-        />
+        <div className="search--person">
+          <input
+            type="text"
+            value={summonerName}
+            onChange={(e) => setSummonerName(e.target.value)}
+            placeholder="Enter summoner name"
+          />
+          <select value={region} onChange={(e) => setRegion(e.target.value)}>
+            <option value="br1">BR1</option>
+            <option value="eun1">EUN1</option>
+            <option value="euw1">EUW1</option>
+            <option value="jp1">JP1</option>
+            <option value="kr">KR</option>
+            <option value="la1">LA1</option>
+            <option value="la2">LA2</option>
+            <option value="na1">NA1</option>
+            <option value="oc1">OC1</option>
+            <option value="tr1">TR1</option>
+            <option value="ru">RU</option>
+            <option value="ph2">PH2</option>
+            <option value="sg2">SG2</option>
+            <option value="th2">TH2</option>
+            <option value="tw2">TW2</option>
+            <option value="vn2">VN2</option>
+          </select>
+        </div>
         <button type="submit">Search</button>
       </form>
     </section>
