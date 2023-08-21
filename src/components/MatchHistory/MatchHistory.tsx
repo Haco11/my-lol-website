4a2d12b636a6fd3d1b05ea9e44736e7160dbe5a3
Haci11 <46707887+Haci11@users.noreply.github.com>
2023-08-21 17:03:25 +0200

    Adding types

--- src/components/MatchHistory/MatchHistory.tsx
+++ src/components/MatchHistory/MatchHistory.tsx
@@ -7,10 +7,10 @@ const MatchHistory = ({ matchData }: any) => {
         <section>
           {matchData?.map((gameData: any, index: number) => (
             <>
-              <h2>Game {index + 1}</h2>
+              <h2 key={index}>Game {index + 1}</h2>
               <div>
                 {gameData.info.participants.map((data: any, i: number) => (
-                  <p>
+                  <p key={i}>
                     Player {i + 1}: {data.summonerName}, KDA:{data.kills}/
                     {data.deaths}/{data.assists}
                   </p>
